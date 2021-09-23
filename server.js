6dccedcb7af3b71c322b9226a46d5bf6efcc9fa9
MyUbuntu <suyog0057@gmail.com>
2021-09-23 16:26:04 +0530

    complete final

--- server.js
+++ server.js
@@ -37,37 +37,58 @@ const ShortUrl = mongoose.model("UrlShortnercollection", shorturlSchema);
 
 app.post("/api/shorturl", async (req, res) => {
   const reqUrl = req.body.url;
-  // TODO : {error "Invalid Hostname"}
+
+  const temp = url.parse(reqUrl, true);
   //check valid url
-  if (validUrl.isUri(reqUrl)) {
+  if ((temp.protocol == 'http:' ||temp.protocol == 'https:') && temp.hostname !== "" && validUrl.isUri(reqUrl)) {
     //if valid url
-    const temp = url.parse(reqUrl, true);
+
     // console.log("Valid url");
     dns.lookup(temp.hostname, async function (err, address, _) {
       if (err) {
-        res.json({ error: "Invalid Hostname" });
+        console.log(err);
+        return res.json({ error: "Invalid Hostname" });
       }
-      if (address.length > 0) {
+      console.log(address);
+      if (address === undefined || address.length > 0) {
         var responseJson = {
           original_url: null,
           short_url: null,
         };
         // valid address
-        var foundUrl = await ShortUrl.findOne({ original_url: reqUrl }).exec();
+        var foundUrl = await ShortUrl.findOne({ original_url: reqUrl })
+          .exec()
+          .catch((err) => {
+            console.log(err);
+          });
         if (foundUrl === null) {
           // no url present with name `reqUrl` so create here
-          var query = await ShortUrl.find({}).sort({ short_url: "desc" });
+          // TODO : for new creating
           responseJson.original_url = reqUrl;
-          responseJson.short_url = query[0].short_url + 1;
-          ShortUrl.create(responseJson);
+          try {
+            var collection = await ShortUrl.find({})
+              .sort({ short_url: -1 })
+              .exec();
+            if (collection.length === 0) {
+              responseJson.short_url = 1;
+            } else {
+              console.log(collection, collection[0].short_url);
+              responseJson.short_url = 1 + collection[0].short_url;
+            }
+            ShortUrl.create(responseJson);
+            console.log("collection containes ", collection.length === 0);
+          } catch (err) {
+            console.log("'.find()' error for DB : ", err);
+          }
         } else {
+          //search with this fields
           responseJson.original_url = foundUrl.original_url;
           responseJson.short_url = foundUrl.short_url;
         }
-        // console.log(foundUrl);
         res.json(responseJson);
+      } else {
+        res.json({ error: "Invalid URL" });
       }
-      res.end();
     });
   } else {
     // if invalid Url
